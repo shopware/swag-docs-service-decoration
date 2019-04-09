e691c151bcc51500803ead666cc29f24bf43185b
Patrick Stahl <p.stahl@shopware.com>
2019-04-09 09:29:15 +0200

    Switch to new structure

--- src/Service/DecoratedService.php
+++ src/Service/DecoratedService.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace ServiceDecoration\Service;
+namespace Swag\ServiceDecoration\Service;
 
 class DecoratedService
 {
---
+++ src/ServiceDecoration.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace Swag\ServiceDecoration;
+
+use Shopware\Core\Framework\Plugin;
+
+class ServiceDecoration extends Plugin
+{
+}
